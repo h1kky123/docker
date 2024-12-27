3f57132e2ffa553dc240aa45d1dc4182b6d02072
h1kky123 <dok_gamer228@list.ru>
2024-12-27 06:29:06 +0000

    Add files via upload

--- dockerByElijah/dBase/init.sql
+++ dockerByElijah/dBase/init.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS users (
     age INTEGER NOT NULL,
     email VARCHAR(100) NOT NULL);
 INSERT INTO users (name, age, email) VALUES
-('Rodion', 19, 'rodionbog1@gmail.com'),
-('Nikita', 19, 'Nruban@gamil.com'),
-('Andre', 19, 'Qubert@gamil.com');
+('Elijah', 19, 'ElijahKarz@gmail.com'),
+('Ruslan', 19, 'Rusik11@gamil.com'),
+('Alex', 19, 'AlexDemyan@gamil.com');
